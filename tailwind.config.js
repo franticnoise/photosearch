73bec93a8641bb4d48bef9f6a9e818257bcea242
franticnoise <z1priano@gmail.com>
2022-09-18 13:41:59 -0600

    tailwild photosearch

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+   
+    extend: {
+      backgroundImage: {
+      'footer-texture': "url('/img/bg-website-image.png')",
+    }},
+    center: true,
+  },
+
+  plugins: [],
+}
